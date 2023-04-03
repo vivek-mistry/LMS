8ef75fb9c1bad97672095dcb033e719baf7764c2
Vivek Mistry <vive.mistry@codeclinic.in>
2023-04-03 18:51:18 +0530

    Receipt generated

--- app/Http/Livewire/ReceiptComponent.php
+++ app/Http/Livewire/ReceiptComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Order;
 use App\Models\OrderDetail;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -10,12 +11,26 @@ class ReceiptComponent extends Component
 {
     use WithPagination;
 
-    public $searchItem;
+    public $searchItem, $view_order_detail;
+
+    public function orderWithDetail($id)
+    {
+        $order = Order::with(['customer', 'order_details'])->where('id', '=', $id)->first();
+
+        $this->view_order_detail = $order;
+
+        $this->dispatchBrowserEvent('show-order-detail');
+    }
+
+    public function printLoanInvoice()
+    {
+        $this->dispatchBrowserEvent('print-order');
+    }
 
     public function render()
     {
         $order_details = OrderDetail::with(['order', 'order.customer'])
-                ->where('is_paid', 1)->orderBy('id', 'DESC')->paginate(10);
+                ->where('is_paid', 1)->orderBy('payment_dt', 'DESC')->paginate(10);
         return view('livewire.receipt-component', ['order_details' => $order_details])->layout('livewire.layout.master');
     }
 }
--- resources/views/livewire/receipt-component.blade.php
+++ resources/views/livewire/receipt-component.blade.php
@@ -1,3 +1,29 @@
+@push('styles')
+    <style>
+        @media screen {
+            #printSection {
+                display: none;
+            }
+        }
+
+        @media print {
+            body * {
+                visibility: hidden;
+            }
+
+            #printSection,
+            #printSection * {
+                visibility: visible;
+            }
+
+            #printSection {
+                position: absolute;
+                left: 0;
+                top: 0;
+            }
+        }
+    </style>
+@endpush
 <div>
     <div class="container">
         <div class="row">
@@ -33,7 +59,7 @@
                                 <th>Purchase Item (Title)</th>
                                 <th>Amount</th>
                                 <th>Payement Date</th>
-
+                                <th>Action</th>
                             </thead>
                             <tbody>
                                 @foreach ($order_details as $key => $order)
@@ -47,7 +73,10 @@
                                         <td>{{ $order->order->title }}</td>
                                         <td>{{ $order->amount }}</td>
                                         <td>{{ $order->payment_dt }}</td>
-
+                                        <td>
+                                            <button class="btn btn-warning"
+                                                wire:click='orderWithDetail({{ $order->order_id }})'>Show Receipt</button>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -60,4 +89,118 @@
             </div>
         </div>
     </div>
+
+    <div id="PrintDiv"></div>
+
+    <div wire:ignore.self class="modal fade " id="viewOrderModal" tabindex="-1" role="dialog"
+        aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Show Order Receipt</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times; </span>
+                    </button>
+                </div>
+                <form>
+                    <div class="modal-body">
+
+                        <h4>Customer</h4>
+                        @isset($view_order_detail)
+
+
+                            <table class="table table-bordered">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Mobile Number</th>
+                                    <th>Mobile Number 2 </th>
+                                    <th>Mobile Number 3 </th>
+                                </tr>
+                                <tr>
+                                    <td>{{ $view_order_detail->customer->name }}</td>
+                                    <td>{{ $view_order_detail->customer->mobile_number }}</td>
+                                    <td>{{ $view_order_detail->customer->pan }}</td>
+                                    <td>{{ $view_order_detail->customer->aadhar }}</td>
+                                </tr>
+                            </table>
+
+                            <h4>Order</h4>
+                            <table class="table table-bordered">
+                                <tr>
+                                    <th>Order ID</th>
+                                    <th>Total Month</th>
+                                    <th>Total Amount</th>
+                                    <th>Per Month Amount</th>
+
+                                </tr>
+                                <tr>
+                                    <td>{{ $view_order_detail->id }}</td>
+                                    <td>{{ $view_order_detail->total_month }}</td>
+                                    <td>{{ $view_order_detail->total_amount }}</td>
+                                    <td>{{ $view_order_detail->per_month_amount }}</td>
+                                </tr>
+                            </table>
+                            <h4>Loan Amount</h4>
+                            <table class="table table-bordered">
+                                <tr>
+                                    <th>Payment Date</th>
+                                    <th>Expected Date</th>
+                                    <th>Amount</th>
+                                    <th>Is Paid?</th>
+                                </tr>
+                                @foreach ($view_order_detail->order_details as $key => $order_detail)
+                                    <tr>
+                                        <td>{{ $order_detail->payment_dt }}</td>
+                                        <td>{{ $order_detail->expected_dt }}</td>
+                                        <td>{{ $order_detail->amount }}</td>
+                                        <td>{{ $order_detail->is_paid == 0 ? 'No' : 'Yes' }}</td>
+
+                                    </tr>
+                                @endforeach
+
+                            </table>
+                        @endisset
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" wire:click="printLoanInvoice">Print</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
+@push('scripts')
+    <script>
+
+        window.addEventListener('show-order-detail', event => {
+            $('#viewOrderModal').modal('show');
+
+        });
+
+        window.addEventListener('print-order', event => {
+
+            var elem = document.getElementById("viewOrderModal")
+
+            var domClone = elem.cloneNode(true);
+
+            var $printSection = document.getElementById("printSection");
+
+            if (!$printSection) {
+                var $printSection = document.createElement("div");
+                $printSection.id = "printSection";
+                document.body.appendChild($printSection);
+            }
+
+            $printSection.innerHTML = "";
+            $printSection.appendChild(domClone);
+            window.print();
+
+
+
+
+        });
+    </script>
+@endpush
